f5c199dbd13a06d7fe8beab2f8dcd4fd59974c83
vb10 <vbacik.10@gmail.com>
2023-09-01 01:56:19 +0300

    added text sheet

---
+++ lib/feature/text_field_bottom_sheet/text_bottom_sheet.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+final class TextBottomModel {
+  final String value;
+  final bool isCheck;
+
+  TextBottomModel({required this.value, required this.isCheck});
+}
+
+class TextBottomSheet extends StatefulWidget {
+  const TextBottomSheet({super.key});
+
+  static Future<TextBottomModel?> show(BuildContext context) {
+    return showModalBottomSheet<TextBottomModel>(
+      context: context,
+      isDismissible: true,
+      builder: (context) {
+        return const TextBottomSheet();
+      },
+    );
+  }
+
+  @override
+  State<TextBottomSheet> createState() => _TextBottomSheetState();
+}
+
+class _TextBottomSheetState extends State<TextBottomSheet> with _TextMixin {
+  static const String _buttonTitle = "Save";
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextField(
+          onChanged: _updateText,
+        ),
+        Checkbox(
+          value: _isCheck,
+          onChanged: _updateCheckBox,
+        ),
+        IgnorePointer(
+          ignoring: !_isValid,
+          child: AnimatedOpacity(
+            duration: const Duration(seconds: 1),
+            opacity: _isValid ? 1 : 0.2,
+            child: ElevatedButton(
+              onPressed: _onButtonPressed,
+              child: const Text(_buttonTitle),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+mixin _TextMixin on State<TextBottomSheet> {
+  bool _isCheck = false;
+  String _textValue = "";
+  bool get _isValid => _isCheck && _textValue.isNotEmpty;
+  void _updateCheckBox(bool? value) {
+    if (value == null) return;
+    setState(() {
+      _isCheck = value;
+    });
+  }
+
+  void _updateText(String value) {
+    setState(() {
+      _textValue = value;
+    });
+  }
+
+  void _onButtonPressed() {
+    if (!_isValid) return;
+    Navigator.pop(
+        context, TextBottomModel(value: _textValue, isCheck: _isCheck));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:twitch_best_practices/feature/status/status_view.dart';
+import 'package:twitch_best_practices/feature/text_field_bottom_sheet/text_field_bottom_sheet_view.dart';
 import 'package:twitch_best_practices/speed_code/rent/rent_view.dart';
 import 'package:twitch_best_practices/speed_code/reset_password/reset_password_view.dart';
 
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Material App',
-      home: ResetPasswordView(),
+      home: TextBottomSheetView(),
     );
   }
 }
