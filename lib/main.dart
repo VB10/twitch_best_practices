a8e5c4c5a7658c5108c1122002b10a07e27878dd
vb10 <vbacik.10@gmail.com>
2023-10-03 02:20:42 +0300

    Added al changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:twitch_best_practices/feature/custom_dialog/ali_keles_View.dart';
-import 'package:twitch_best_practices/feature/status/status_view.dart';
-import 'package:twitch_best_practices/feature/text_field_bottom_sheet/text_field_bottom_sheet_view.dart';
-import 'package:twitch_best_practices/speed_code/authentic/authentic_on_board.dart';
-import 'package:twitch_best_practices/speed_code/chat_william/chat_william_view.dart';
+import 'package:twitch_best_practices/speed_code/ice_cream/ice_cream_view.dart';
 import 'package:twitch_best_practices/speed_code/lang_app/lang_app_view.dart';
-import 'package:twitch_best_practices/speed_code/rent/rent_view.dart';
-import 'package:twitch_best_practices/speed_code/reset_password/reset_password_view.dart';
 
 void main() => runApp(const MyApp());
 
@@ -17,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Material App',
-      home: LangAppView(),
+      home: IceCreamView(),
     );
   }
 }
